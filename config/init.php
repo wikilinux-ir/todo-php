df50dad04907b93e8322fbe98de6a320285e7282
wikilinux-ir <wikilinux.ir@gmail.com>
2021-09-14 01:40:44 +0430

    remove useless files

--- config/init.php
+++ config/init.php
@@ -2,7 +2,6 @@
 session_start();
 include_once "consts.php";
 include "db.php";
-include "/srv/train/todo/vendor/autoload.php";
 include "/srv/train/todo/config/helpers.php";
 include "/srv/train/todo/tasks/auth.php";
 include "/srv/train/todo/tasks/tasks.php";
